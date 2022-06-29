256626292eb11360e31790d270560ec860e95c2c
Yelhie <maelle.ew.m@gmail.com>
2022-06-29 17:57:41 +0200

    Création du système d’authentification

--- src/Repository/ParticipantRepository.php
+++ src/Repository/ParticipantRepository.php
@@ -5,9 +5,11 @@ namespace App\Repository;
 use App\Entity\Participant;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bridge\Doctrine\Security\User\UserLoaderInterface;
 use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\PasswordUpgraderInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @extends ServiceEntityRepository<Participant>
@@ -17,8 +19,19 @@ use Symfony\Component\Security\Core\User\PasswordUpgraderInterface;
  * @method Participant[]    findAll()
  * @method Participant[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
-class ParticipantRepository extends ServiceEntityRepository implements PasswordUpgraderInterface
-{
+class ParticipantRepository extends ServiceEntityRepository implements UserLoaderInterface {
+
+    public function loadUserByIdentifier(string $usernameOrEmail): ?Participant {
+        $entityManager = $this->getEntityManager();
+
+
+        return $entityManager->createQuery(
+            'SELECT u FROM App\Entity\Participant u WHERE u.pseudo = :query OR u.email = :query'
+        )
+            ->setParameter('query', $usernameOrEmail)
+            ->getOneOrNullResult();
+    }
+
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Participant::class);
@@ -80,4 +93,8 @@ class ParticipantRepository extends ServiceEntityRepository implements PasswordU
 //            ->getOneOrNullResult()
 //        ;
 //    }
+    public function loadUserByUsername(string $username)
+    {
+        // TODO: Implement loadUserByUsername() method.
+    }
 }
